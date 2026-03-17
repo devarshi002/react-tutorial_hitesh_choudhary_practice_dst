5968ceea34b6ea7b5031867818509cfa4cc83850
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-03-18 00:15:01 +0530

    buy or sell

---
+++ dsa_js/buyOrSell.js
@@ -0,0 +1,19 @@
+function maxProfit(prices) {
+    let minPrice = Infinity
+    let maxProfit = 0
+
+    for (let i=0; i<prices.length; i++) {
+        if(prices[i] < minPrice) {
+            minPrice=prices[i]
+        } else {
+            let profit = prices[i] - minPrice
+            if(profit > maxProfit) {
+                maxProfit = profit
+            }
+        }
+    }
+
+    return maxProfit
+}
+
+console.log(maxProfit([7,1,5,3,6,4]));
