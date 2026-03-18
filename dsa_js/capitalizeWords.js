271b359889f42ea02c349f3823fa83ed79431efd
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-03-18 15:52:13 +0530

    js interview trap questions

---
+++ dsa_js/capitalizeWords.js
@@ -0,0 +1,16 @@
+function capitalizeWords(str) {
+    let words = str.split(" ")
+    let result = []
+
+    for (let i = 0; i< words.length; i++) {
+        let word = words[i]
+
+        let capitalized = word[0].toUpperCase() + word.slice(1);
+
+        result.push(capitalized)
+    }
+
+    return result.join(" ")
+}
+
+console.log(capitalizeWords("javascript is fun"));
