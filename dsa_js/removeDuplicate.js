3965b17605e932879defce8073770ac749a0af0f
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-03-20 15:13:17 +0530

    using two pointer approch

--- dsa_js/removeDuplicate.js
+++ dsa_js/removeDuplicate.js
@@ -17,3 +17,18 @@ function removeDuplicate(nums) {
 console.log(removeDuplicate([1,1,1,2,2,3]));
 
 
+
+
+function twoSum(nums, target) {
+    
+
+    for (let i = 0; i<nums.length; i++) {
+        for (let j = i+1; j<nums.length; j++) {
+            if(nums[i] + nums[j] == target) {
+                return [i,j]
+            }
+        }
+    }
+}
+
+console.log(twoSum([2,7,4,5], 9))
