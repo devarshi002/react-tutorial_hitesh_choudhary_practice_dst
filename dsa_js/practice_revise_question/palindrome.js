502f533a3f20e547c3cdce9309ccd89e19351b17
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-03-21 10:06:15 +0530

    palindrome

---
+++ dsa_js/practice_revise_question/palindrome.js
@@ -0,0 +1,16 @@
+function IsPalindrome(str) {
+    let left = 0
+    let right = str.length-1
+
+    while (left < right) {
+        if(str[left] !== str[right]) {
+            return false
+        }
+        left++
+        right--
+    }
+    return true
+}
+
+console.log(IsPalindrome("madam"));
+console.log(IsPalindrome("babu"));
