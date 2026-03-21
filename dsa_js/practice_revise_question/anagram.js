5abdf477ceb841f1431d8ac7af7945a3a024e060
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-03-21 10:30:11 +0530

    anagram

---
+++ dsa_js/practice_revise_question/anagram.js
@@ -0,0 +1,6 @@
+function anagram(str1, str2) {
+    return str1.split("").sort().join("") === str2.split("").sort().join("")
+}
+
+console.log(anagram("hello", "world"))
+console.log(anagram("listen", "silent"))
