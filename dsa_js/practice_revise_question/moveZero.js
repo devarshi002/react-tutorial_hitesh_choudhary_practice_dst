0c1151402bcbb59e929585b4c1eebc9cd44756da
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-03-21 10:48:07 +0530

    move zero

---
+++ dsa_js/practice_revise_question/moveZero.js
@@ -0,0 +1,17 @@
+function moverZero(nums) {
+    let pos =0
+
+    for (let i=0; i<nums.length; i++) {
+        if(nums[i] !== 0) {
+            nums[pos]=nums[i]
+            pos++
+        }
+    }
+
+    for (let i=pos; i<nums.length; i++) {
+        nums[i]=0
+    }
+    return nums
+}
+
+console.log(moverZero([0,1,2,3,4,0]));
