b47213116da21b3251440d0f13ddda7ec6c3d237
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-03-29 11:59:28 +0530

    clouser

---
+++ interview_coding_questions/clouser.js
@@ -0,0 +1,15 @@
+function outer(){
+    let count =0;
+    
+    return function inner() {
+        count++;
+        return count
+    }
+}
+
+const fn=outer()
+console.log(fn());
+console.log(fn());
+console.log(fn());
+console.log(fn());
+ 
