fa2e1d0312b119f5d96a8621bf8a8c340d6ed97d
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-03-30 11:35:56 +0530

    every method

---
+++ interview_coding_questions/everyMethod.js
@@ -0,0 +1,9 @@
+const nums = [2,4,6]
+
+const result = nums.every(n=>n%2==0);
+
+console.log(result);
+
+
+//every method - all must be true 
+// return true only if all elements satisfy the condition
