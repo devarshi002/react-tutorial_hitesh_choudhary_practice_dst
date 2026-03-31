6ab299cabda99ecf28670caa52b9af55eb80cbb4
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-03-31 12:11:10 +0530

    let block scope question

--- interview_coding_questions/question1.js
+++ interview_coding_questions/question1.js
@@ -1,13 +1,30 @@
-{
+// {
+//     let a =1;
+//     let b=2;
+//     var c = 3;
+
+//     console.log(a);
+//     console.log(b);
+    
+    
+// }
+// console.log(c);
+
+// console.log(a);
+// console.log(b);
+
+
+function abc() {
     let a =1;
     let b=2;
-    var c = 3;
+    c = 3;
 
     console.log(a);
     console.log(b);
     
-    
 }
+
+abc();
 console.log(c);
 
 console.log(a);
