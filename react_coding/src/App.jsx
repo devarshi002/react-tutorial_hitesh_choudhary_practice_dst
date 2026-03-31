afaa62d727a43241422b1dfbbc14edaf7e596f8c
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-03-31 18:07:53 +0530

    practice todo

--- react_coding/src/App.jsx
+++ react_coding/src/App.jsx
@@ -2,12 +2,14 @@ import Counter from "./component/Counter"
 import Debounce from "./component/Debounce"
 import PracticeDebouce from "./component/PracticeDebounce"
 import Todo from "./component/Todo"
+import PracticeTodo from "./component/PracticeTodo"
 function App() {
 
   return (
     <>
     <Counter />
     <Debounce />
+    <PracticeTodo/>
     <PracticeDebouce/>
     <Todo/>
     </>
---
+++ react_coding/src/component/PracticeTodo.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+
+export default function PracticeTodo() {
+    const [task, setTask] = useState("")
+    const [list, setList] = useState([])
+
+    const addTask = () => {
+        setList([...list, task])
+        setTask(""); 
+    }
+
+    return (
+        <div>
+            <input type="text"
+            value={task}
+            onChange={(e)=>setTask(e.target.value)} />
+            <button onClick={addTask}>Add</button>
+            
+            {list.map((item, i)=>(
+                <li key={i}>{item}</li>
+            ))}
+        </div>
+    )
+}
