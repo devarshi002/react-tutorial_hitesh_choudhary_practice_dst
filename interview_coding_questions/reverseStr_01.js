9e8387c84f192b3aff31b5567218dd65b7ec80e1
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-04-01 10:44:08 +0530

    reverse str

---
+++ interview_coding_questions/reverseStr_01.js
@@ -0,0 +1,17 @@
+const str = "Hello"
+
+const result = str.split("").reverse("").join("")
+
+console.log(result);
+
+
+function reverseString(str) {
+    let result = ""
+
+    for(let i=str.length-1; i>=0; i--) {
+        result += str[i]
+    }
+    return result
+}
+
+console.log(reverseString("babu"));
