b3ddc6e39fbaed0a1a13d186324e914dc4190e33
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-04-04 10:55:05 +0530

    tree is same or not

---
+++ dsa_js/Tree/leetcode100.js
@@ -0,0 +1,29 @@
+//same tree
+
+var isSameTree = function(p,q){
+    if(p===null && q=== null ) return true;
+
+    if(p===null || q=== null) return false;
+
+    if(p.val !== q.val) return false;
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+}
+
+
+function TreeNode(val, left=null, right=null){
+    this.val=val;
+    this.left=left;
+    this.right=right;
+}
+
+let p = new TreeNode(1);
+p.right= new TreeNode(3);
+p.left = new TreeNode(2);
+
+let q = new TreeNode(1);
+q.left= new TreeNode(2);
+q.right= new TreeNode(2)
+
+console.log(isSameTree(p,q));
+
