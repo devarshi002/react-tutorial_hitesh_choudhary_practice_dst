5291e2a37f93bf6624bde6d2c67851fd03eda29f
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-04-24 19:59:04 +0530

    basic bug

---
+++ error_resolution_practice/bug3.js
@@ -0,0 +1,10 @@
+const user = {
+    name : "dev"
+}
+
+console.log(user.age.toUpperCase());
+
+//user.age = undefined and 
+// undefined.toUpperCase() give typeerror
+// we can add age or use optional chaining
+//or provide feedback 
