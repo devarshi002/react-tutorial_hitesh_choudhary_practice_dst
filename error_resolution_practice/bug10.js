9595aa584fac0ebedfc4fa3bc2f0fc8815a1e10e
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-04-24 21:09:43 +0530

    bug fix

---
+++ error_resolution_practice/bug10.js
@@ -0,0 +1,11 @@
+const data = {
+    user : {
+        profile : {
+            name : {
+                first : "da"
+            }
+        }
+    }
+}
+
+console.log(data.user.profile.name.first); //u can use ? marks 
