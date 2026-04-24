fda4c318c5bd553203ad67205263aa0471d06763
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-04-24 19:55:54 +0530

    basic bug

---
+++ error_resolution_practice/bug2.js
@@ -0,0 +1,8 @@
+let arr = [1,2,3]
+// for(let i = 0; i<=arr.length; i++) {
+for(let i = 0; i<arr.length; i++){
+    console.log(arr[i].toString());
+    
+}
+
+//here i<= arr.length so it will give undefined u should use i<=arr.length - 1 or i < arr.length
